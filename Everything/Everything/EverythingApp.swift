4b97998601e219ebe11abe2e87c9b1b0da56b4c6
Patrick <pmcgonigle@gmail.com>
2024-04-18 15:28:48 -0500

    Initial project skeleton
    Per Xcode 15.3

---
+++ Everything/Everything/EverythingApp.swift
@@ -0,0 +1,32 @@
+//
+//  EverythingApp.swift
+//  Everything
+//
+//  Created by Patrick McGonigle on 4/18/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EverythingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
