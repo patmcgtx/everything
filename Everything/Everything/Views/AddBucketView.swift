f373ef6d47c16a09c2ee8d880c32280920a63d1e
Patrick <pmcgonigle@gmail.com>
2024-05-28 16:40:59 -0500

    ET-3 Auto-focus title field when presented

--- Everything/Everything/Views/AddBucketView.swift
+++ Everything/Everything/Views/AddBucketView.swift
@@ -11,21 +11,40 @@ import SwiftData
 
 struct AddBucketView: View {
     
+    // Persistence
+    
     @Environment(\.modelContext) private var modelContext
+
+    // Presentation
     
     @Binding var isPresented: Bool
     
+    // Internal state
+    
     @State private var titleValue: String = ""
     @State private var aboutValue: String = ""
+    
+    // Field focus
+    
+    enum Field {
+        case title, about
+    }
+    
+    @FocusState private var focus: Field?
 
+    // Presentation
+    
     var body: some View {
         NavigationStack {
             Form {
                 Section(header: Text("Title")) {
                     TextField("Title", text: self.$titleValue)
+                        .focused(self.$focus, equals: .title)
                 }
+                
                 Section(header: Text("About")) {
                     TextField("About", text: self.$aboutValue)
+                        .focused(self.$focus, equals: .about)
                 }
             }
             .navigationTitle("New Bucket")
@@ -42,6 +61,9 @@ struct AddBucketView: View {
                         self.isPresented = false
                     }
                 }
+            }
+            .onAppear {
+                self.focus = .title
             }
         }
     }
